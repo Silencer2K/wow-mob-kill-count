c8dc3615dd4585651ffe1e07f10729fdd8a994ca
Silencer <silencer2k@gmail.com>
2014-08-25 18:01:21 +0200

    counter logic gix

--- MobKillCount.lua
+++ MobKillCount.lua
@@ -13,52 +13,19 @@ function addon:OnInitialize()
 		addon:OnCombatEvent(...)
 	end)
 
+	self:RegisterEvent('ZONE_CHANGED_NEW_AREA', function(...)
+		addon:OnZoneChanged(...)
+	end)
+
 	GameTooltip:HookScript('OnTooltipCleared', function(self)
 		addon:OnGameTooltipCleared(self)
-	end);
+	end)
 
 	GameTooltip:HookScript('OnTooltipSetUnit', function(self)
 		addon:OnGameTooltipSetUnit(self)
-	end);
-end
+	end)
 
-function addon:OnEnable()
-end
-
-function addon:OnDisable()
-end
-
-function addon:UpdateMobKillCountInDb(id, db)
-	if db.killCount == nil then
-		db.killCount = {}
-	end
-
-	if db.killCount[id] == nil then
-		db.killCount[id] = 1
-	else
-		db.killCount[id] = db.killCount[id] + 1
-	end
-
-	return db.killCount[id]
-end
-
-function addon:GetMobKillCountFromDb(id, db)
-	if db.killCount == nil then
-		db.killCount = {}
-	end
-	if db.killCount[id] == nil then
-		return 0
-	end
-
-	return db.killCount[id]
-end
-
-function addon:UpdateMobKillCount(id)
-	self:UpdateMobKillCountInDb(id, self.db.char)
-	self:UpdateMobKillCountInDb(id, self.db.global)
-
-	self:UpdateMobKillCountInDb(0, self.db.char)
-	self:UpdateMobKillCountInDb(0, self.db.global)
+	self.mobCache = {}
 end
 
 function addon:UnitInfoFromGuid(guid)
@@ -93,21 +60,39 @@ function addon:GetPlayerName()
 	)
 end
 
+function addon:OnEnable()
+end
+
+function addon:OnDisable()
+end
+
 function addon:OnCombatEvent(event, timeStamp, logEvent, hideCaster,
                              sourceGuid, sourceName, sourceFlags, sourceFlags2,
                              destGuid, destName, destFlags, destFlags2, ...
 )
-	if logEvent == 'PARTY_KILL' then
-		-- print(event, timeStamp, logEvent, hideCaster,
-		--       sourceGuid, sourceName, sourceFlags, sourceFlags2,
-		--       destGuid, destName, destFlags, destFlags2, ...
-		-- )
+	local type, id = self:UnitInfoFromGuid(destGuid)
 
-		local type, id = self:UnitInfoFromGuid(destGuid)
-		if type == 3 then
-			self:UpdateMobKillCount(id)
+	if type == 3 then
+		if logEvent:match('_DAMAGE$') then
+			if sourceGuid == UnitGUID('player') then
+				-- print(event, timeStamp, logEvent, hideCaster,
+				--       sourceGuid, sourceName, sourceFlags, sourceFlags2,
+				--       destGuid, destName, destFlags, destFlags2, ...
+				-- )
+
+				self.mobCache[destGuid] = 1
+			end
+
+		elseif logEvent == 'UNIT_DIED' or logEvent == 'PARTY_KILL' then
+			if self.mobCache[destGuid] then
+				self:IncMobKillCount(id)
+			end
 		end
 	end
+end
+
+function addon:OnZoneChanged()
+	self.mobCache = {}
 end
 
 function addon:OnGameTooltipCleared(tooltip)
@@ -141,4 +126,37 @@ function addon:OnGameTooltipSetUnit(tooltip)
 			end
 		end
 	end
+end
+
+function addon:IncMobKillCountInDb(id, db)
+	if db.killCount == nil then
+		db.killCount = {}
+	end
+
+	if db.killCount[id] == nil then
+		db.killCount[id] = 1
+	else
+		db.killCount[id] = db.killCount[id] + 1
+	end
+
+	return db.killCount[id]
+end
+
+function addon:GetMobKillCountFromDb(id, db)
+	if db.killCount == nil then
+		db.killCount = {}
+	end
+	if db.killCount[id] == nil then
+		return 0
+	end
+
+	return db.killCount[id]
+end
+
+function addon:IncMobKillCount(id)
+	self:IncMobKillCountInDb(id, self.db.char)
+	self:IncMobKillCountInDb(id, self.db.global)
+
+	self:IncMobKillCountInDb(0, self.db.char)
+	self:IncMobKillCountInDb(0, self.db.global)
 end
